de61ac0aaf353f6af445fd5e9b05f1516a555cca
bruno <brunoazizsm@gmail.com>
2023-04-26 09:56:12 -0300

    estava errado um belongs_to

--- Active-Records/matricula.rb
+++ Active-Records/matricula.rb
@@ -4,6 +4,5 @@ ActiveRecord::Base.establish_connection :adapter => "sqlite3",
                                         :database => "Tabelas.sqlite3"
 
 class Matricula < ApplicationRecord::Base;
-    belongs_to :materia
     belongs_to :aluno
 end

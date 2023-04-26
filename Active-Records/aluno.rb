613226192599896971c385d92c2afe81a4c9121f
bruno <brunoazizsm@gmail.com>
2023-04-26 09:41:19 -0300

    mudei a logica das tabelas para uma faculdade

--- Active-Records/aluno.rb
+++ Active-Records/aluno.rb
@@ -3,7 +3,7 @@ require 'active_record'
 ActiveRecord::Base.establish_connection :adapter => "sqlite3",
                                         :database => "Tabelas.sqlite3"
 
-class Aluno < ApplicationRecord::Base
-    has_one: matricula
+class Aluno < ApplicationRecord::Base;
+    has_one :matricula
     has_many :materias
 end 
--- Active-Records/materias.rb
+++ Active-Records/materias.rb
@@ -3,7 +3,7 @@ require 'active_record'
 ActiveRecord::Base.establish_connection :adapter => "sqlite3",
                                         :database => "Tabelas.sqlite3"
 
-class Materias < ApplicationRecord::Base
+class Materias < ApplicationRecord::Base;
     belongs_to :departamento
     has_many :alunos
 end                                                        

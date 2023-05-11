01d2511451a3192645845cd05927a01186005509
bruno <brunoazizsm@gmail.com>
2023-05-11 09:22:17 -0300

    desenhando linha e botao no centro

--- canvasJS/canvas.js
+++ canvasJS/canvas.js
@@ -1,26 +1,30 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-function desenharLinha() {
+function Linha() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var canvasCenterX = canvas.width / 2;
+  var canvasCenterY = canvas.height / 2;
   ctx.beginPath();
-  ctx.moveTo(50, 50);
-  ctx.lineTo(200, 200);
+  ctx.moveTo(canvasCenterX - 75, canvasCenterY - 75);
+  ctx.lineTo(canvasCenterX + 75, canvasCenterY + 75);
   ctx.stroke();
 }
 
-function desenharPoligono() {
+function Poligono() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var canvasCenterX = canvas.width / 2;
+  var canvasCenterY = canvas.height / 2;
   ctx.beginPath();
-  ctx.moveTo(50, 50);
-  ctx.lineTo(200, 50);
-  ctx.lineTo(125, 200);
+  ctx.moveTo(canvasCenterX - 75, canvasCenterY - 75);
+  ctx.lineTo(canvasCenterX + 75, canvasCenterY - 75);
+  ctx.lineTo(canvasCenterX, canvasCenterY + 75);
   ctx.closePath();
   ctx.stroke();
 }
 
 var linhaBotao = document.getElementById("linhaBotao");
-linhaBotao.addEventListener("click", desenharLinha);
+linhaBotao.addEventListener("click", Linha);
 
 var poligonoBotao = document.getElementById("poligonoBotao");
-poligonoBotao.addEventListener("click", desenharPoligono);
+poligonoBotao.addEventListener("click", Poligono);

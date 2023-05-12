c628d45853a2ffc59fa39527cd83bb8abb8db34d
bruno <brunoazizsm@gmail.com>
2023-05-11 21:13:37 -0300

    linha mais grossa

--- canvasJS/canvas.js
+++ canvasJS/canvas.js
@@ -1,13 +1,14 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-function Linha() {
+function Reta() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   var canvasCenterX = canvas.width / 2;
   var canvasCenterY = canvas.height / 2;
   ctx.beginPath();
   ctx.moveTo(canvasCenterX - 75, canvasCenterY - 75);
   ctx.lineTo(canvasCenterX + 75, canvasCenterY + 75);
+  ctx.lineWidth = 5;
   ctx.stroke();
 }
 
@@ -18,13 +19,16 @@ function Poligono() {
   ctx.beginPath();
   ctx.moveTo(canvasCenterX - 75, canvasCenterY - 75);
   ctx.lineTo(canvasCenterX + 75, canvasCenterY - 75);
+  ctx.lineWidth = 5;
   ctx.lineTo(canvasCenterX, canvasCenterY + 75);
   ctx.closePath();
   ctx.stroke();
 }
 
-var linhaBotao = document.getElementById("linhaBotao");
-linhaBotao.addEventListener("click", Linha);
+var RetaBotao = document.getElementById("RetaBotao");
+RetaBotao.addEventListener("click", Reta);
 
 var poligonoBotao = document.getElementById("poligonoBotao");
 poligonoBotao.addEventListener("click", Poligono);
+
+Reta();

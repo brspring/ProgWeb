841272856abad156c818be1c651c2e7c2e5ab479
bruno <brunoazizsm@gmail.com>
2023-05-15 11:51:46 -0300

    reta movendo com clique do mouse

--- canvasJS/canvas.js
+++ canvasJS/canvas.js
@@ -2,41 +2,94 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 function Reta() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var canvasCenterX = canvas.width / 2;
-    var canvasCenterY = canvas.height / 2;
-    
-    var startPointX = canvasCenterX - 75;
-    var startPointY = canvasCenterY - 75;
-    var endPointX = canvasCenterX + 75;
-    var endPointY = canvasCenterY + 75;
-    var midPointX = (startPointX + endPointX) / 2;
-    var midPointY = (startPointY + endPointY) / 2;
-    
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Definir as propriedades do objeto 'reta'
+  var reta = {
+    x: 100,
+    y: 100,
+    rotation: 0,
+    width: 150,
+    height: 150,
+
+    centerX: function() {
+      return this.x + this.width / 2;
+    },
+
+    centerY: function() {
+      return this.y + this.height / 2;
+    }
+  };
+
+  function drawReta() {
+    var midPointX = reta.centerX();
+    var midPointY = reta.centerY();
+    var startPointX = midPointX;
+    var startPointY = midPointY - 75;
+    var endPointX = midPointX;
+    var endPointY = midPointY + 75;
+
     ctx.beginPath();
     ctx.moveTo(startPointX, startPointY);
     ctx.lineTo(midPointX, midPointY);
     ctx.lineTo(endPointX, endPointY);
-    
-    ctx.lineWidth = 5;
+
+    ctx.lineWidth = 4;
     ctx.stroke();
-    
+
     // Desenhar os pontos
     ctx.beginPath();
-    ctx.arc(startPointX, startPointY, 5, 0, Math.PI * 2);
+    ctx.arc(startPointX, startPointY, 4, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
-    
+
     ctx.beginPath();
-    ctx.arc(midPointX, midPointY, 5, 0, Math.PI * 2);
+    ctx.arc(midPointX, midPointY, 4, 0, Math.PI * 2);
     ctx.fillStyle = "blue";
     ctx.fill();
-    
+
     ctx.beginPath();
-    ctx.arc(endPointX, endPointY, 5, 0, Math.PI * 2);
+    ctx.arc(endPointX, endPointY, 4, 0, Math.PI * 2);
     ctx.fillStyle = "red";
     ctx.fill();
   }
+
+  drawReta(); // Desenhar a reta inicialmente
+
+  // Adicionar evento de clique ao canvas
+  canvas.addEventListener("mousedown", function(event) {
+    var offsetX = event.clientX - canvas.getBoundingClientRect().left;
+    var offsetY = event.clientY - canvas.getBoundingClientRect().top;
+
+    // Verificar se o clique ocorreu no ponto médio
+    if (
+      offsetX >= reta.centerX() - 5 &&
+      offsetX <= reta.centerX() + 5 &&
+      offsetY >= reta.centerY() - 5 &&
+      offsetY <= reta.centerY() + 5
+    ) {
+      // Atualizar as coordenadas do objeto 'reta' com base no deslocamento do mouse
+      canvas.addEventListener("mousemove", moveReta);
+
+      canvas.addEventListener("mouseup", function() {
+        canvas.removeEventListener("mousemove", moveReta);
+      });
+    }
+  });
+
+  function moveReta(event) {
+    var offsetX = event.clientX - canvas.getBoundingClientRect().left;
+    var offsetY = event.clientY - canvas.getBoundingClientRect().top;
+
+    reta.x = offsetX - reta.width / 2;
+    reta.y = offsetY - reta.height / 2;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Redesenhar a reta com as novas coordenadas
+    drawReta();
+  }
+}
   
 
 function Poligono() {
